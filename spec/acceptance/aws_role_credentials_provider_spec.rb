843d34c528c057695c960008bef7cdaaa13decf1
Joe Teibel <joe.teibel@nike.com>
2017-03-28 14:25:41 -0700

    updating acceptance tests with more instructions and generic test data

--- spec/acceptance/aws_role_credentials_provider_spec.rb
+++ spec/acceptance/aws_role_credentials_provider_spec.rb
@@ -3,17 +3,24 @@ require_relative('../../lib/cerberus/exception/no_value_error')
 require_relative('../fake_test_provider_chain')
 require_relative('hardcoded_testing_url_resolver')
 
+##
+# This is an acceptance test to validate that the client is working properly
+# ON AN EC2 INSTANACE - it does NOT run by default using rspec
+#
+# export AWS_CREDS_PROVIDER_TEST=true then run this test using rspec to validate
+#
+# This test has several assumptions:
+#   1) Assumes HardcodedTestingUrlResolver resolves to a valid Cerberus instance
+#   2) That it is running on an AWS instance with access to the AWS metadata service
+#   3) That the instance has a role assigned to it in an account that has been setup in Cerberus
+#   4) That the bucket in Cerberus has the path and value being tested for in the "read" acceptance test
+#        You will likely need to change this to match YOUR SDB setup
+##
+
 if ENV["AWS_CREDS_PROVIDER_TEST"] == 'true'
 
   describe Cerberus::AwsRoleCredentialsProvider do
 
-    ##
-    # This test has several assumptions:
-    #   1) Assumes HardcodedTestingUrlResolver resolves to a valid Cerberus instance
-    #   2) That it is running on an AWS instance with access to the AWS metadata service
-    #   3) That the instance has a role assigned to it in an account that has been setup in Cerberus
-    #   4) That the bucket in Cerberus has the path and value being tested for in the "read" acceptance test
-    ##
     context "test getCredentials" do
       it "should try to get credentials" do
 
@@ -25,9 +32,10 @@ if ENV["AWS_CREDS_PROVIDER_TEST"] == 'true'
     end
 
     context "read a value acceptance test" do
-      it "should read from TEST using AWS role auth" do
 
-        path = "app/artemis-events/local"
+      it "should read from Cerberus using AWS role auth" do
+
+        path = "app/your-app/local"
         key = value = "test"
 
         vaultClient = CerberusClient::getVaultClientWithUrlResolver(HardcodedTestingUrlResolver.new)
